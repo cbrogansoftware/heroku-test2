2bc57ad2992252c7d72ac8095b0112431e21196c
cbrogansoftware <cbrogansoftware@gmail.com>
2022-10-12 15:48:18 -0500

    Added system.properties file to see if this fixes the deploy error.

--- src/main/java/com/cbrogansoftware/herokutest2/controller/HelloController.java
+++ src/main/java/com/cbrogansoftware/herokutest2/controller/HelloController.java
@@ -1,2 +1,14 @@
-package com.cbrogansoftware.herokutest2.controller;public class HelloController {
+package com.cbrogansoftware.herokutest2.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController
+{
+    @RequestMapping("/")
+    public String hello()
+    {
+        return "Hello Heroku World!";
+    }
 }
